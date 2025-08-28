b80c4772efe094b4a252d6857e9f388f99d21bfe
anuoua <anuoua@gmail.com>
2025-08-28 19:46:14 +0800

    fix: resolveUrl error

--- packages/core/src/utils/resolveUrl.ts
+++ packages/core/src/utils/resolveUrl.ts
@@ -2,14 +2,16 @@ export const resolveUrl = (
   targetUrl: string | URL | undefined | null,
   baseUrl: string
 ) => {
-  if (!targetUrl)
-    return `${location.pathname}${location.search}${location.hash}`;
-
   const baseChunks = baseUrl.split("/").filter((i) => !!i);
 
-  const resolvedUrl = new URL(targetUrl, location.href);
-
   const prefix = "/" + baseChunks.join("/");
+
+  if (!targetUrl)
+    return `${prefix === "/" ? "" : prefix}${location.pathname}${
+      location.search
+    }${location.hash}`;
+
+  const resolvedUrl = new URL(targetUrl, location.href);
 
   return `${prefix === "/" ? "" : prefix}${resolvedUrl.pathname}${
     resolvedUrl.search
